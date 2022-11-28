cc2e1d38bfde762c9c90c8b4531f93e847b8766d
joseilagosb <joseilagosb@gmail.com>
2022-11-27 23:24:51 -0300

    segunda activity quotes

---
+++ app/src/main/java/com/example/grandespensadores/CustomQuotesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.grandespensadores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomQuotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_quotes)
+    }
+}
--- app/src/main/java/com/example/grandespensadores/MainActivity.kt
+++ app/src/main/java/com/example/grandespensadores/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.grandespensadores
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seeMoreButton = findViewById<Button>(R.id.see_more_button)
+        val breakingBadQuotesButton = findViewById<CardView>(R.id.breaking_bad_quotes_button)
+        val kanyeWestQuotesButton = findViewById<CardView>(R.id.kanye_west_quotes_button)
+        val cheerUpQuotesButton = findViewById<CardView>(R.id.cheer_up_quotes_button)
+
+        seeMoreButton.setOnClickListener {
+            Toast.makeText(it.context, "Pagina en construccion, nos puedes contactar desde nuestra página oficial", Toast.LENGTH_LONG).show()
+        }
+        breakingBadQuotesButton.setOnClickListener {
+            Toast.makeText(it.context, "Breaking bad frases", Toast.LENGTH_LONG).show()
+        }
+        kanyeWestQuotesButton.setOnClickListener {
+            val intent = Intent(this, CustomQuotesActivity::class.java)
+            startActivity(intent)
+        }
+        cheerUpQuotesButton.setOnClickListener {
+            Toast.makeText(it.context, "Frases para alegrarte", Toast.LENGTH_LONG).show()
+        }
     }
 }
